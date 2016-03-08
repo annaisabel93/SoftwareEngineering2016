3902e5f785d9f725a701d367fa1f62d71983c696
Ana Pardal <ist175202@tecnico.ulisboa.pt>
2016-03-08 21:22:58 +0000

    [UPDATE] inicio do import/export xml na classe User

--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -1,5 +1,12 @@
 package pt.tecnico.myDrive.domain;
 
+import org.jdom2.Document;
+
+import java.io.UnsupportedEncodingException;
+
+import org.jdom2.Attribute;
+import org.jdom2.Element;
+
 public class User extends User_Base {
     
     public User(FileSystem filesystem, String name, String username, String password, byte[] mask, String homeDir, boolean isRoot) {
@@ -13,6 +20,12 @@ public class User extends User_Base {
         setSystem(filesystem);
     }
     
+    public User(FileSystem filesystem, Document xml){
+    	super();
+    	xmlImport(xml);
+    	setSystem(filesystem);
+    }
+    
     @Override
     public void setSystem(FileSystem fs){
     	if(fs == null)
@@ -20,5 +33,32 @@ public class User extends User_Base {
     	else{
     		fs.addUser(this);
     	}
+    }
+    
+    public void xmlImport(Document userDoc){
+    	
+        try{
+        	setUserName(new String(userDoc.getRootElement().getAttribute("username").getValue().getBytes("UTF-8")));
+        	setName(new String(userDoc.getRootElement().getChild("name").getValue().getBytes("UTF-8")));
+        	setPassword(new String(userDoc.getRootElement().getChild("password").getValue().getBytes("UTF-8")));
+        	setHomeDir(new String(userDoc.getRootElement().getChild("homeDir").getValue().getBytes("UTF-8")));
+        }catch(UnsupportedEncodingException e) { System.err.println(e); }	
+        setMask(userDoc.getRootElement().getChild("mask").getValue().getBytes());
+        
+        
+        
+    }
+
+    public Document xmlExport(){
+    	Element element = new Element("User");
+    	element.setAttribute("username", getUserName()); 	
+    	element.setAttribute("name", getName());
+    	element.setAttribute("password", getPassword());
+    	element.setAttribute("homeDir", getHomeDir());
+    	element.setAttribute("mask", getMask().toString());
+    	
+    	Document document = new Document(element);
+    	
+        return document; 
     }
 }
