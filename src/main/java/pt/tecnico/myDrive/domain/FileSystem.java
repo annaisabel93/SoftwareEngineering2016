1dbc39b35b049e6f9e1353abc7f91e7af5766cd2
Luis Santos <lmhbs1@gmail.com>
2016-03-08 23:56:34 +0000

    Adicionado construtor de inicializacao no FileSystem.java e o inicio de uma funcao para fins de teste. Tentei inicializar qualquer classe no Main.java mas da sempre null pointer. Embora nao seja para fazer isso, acho que dava jeito para testar funcoes que fossemos correndo, se alguem conseguir.

--- src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
+++ src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
@@ -2,12 +2,51 @@ package pt.tecnico.myDrive.domain;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.joda.time.DateTime;
+
+import java.util.Scanner;
 
 import pt.ist.fenixframework.FenixFramework;
 
 public class FileSystem extends FileSystem_Base {
     static final Logger log = LogManager.getRootLogger();
+    
+    
+    public Directory workingDir;
+    
+    public FileSystem(){
+    	super();
+    	byte[] array = {0,0,0,0};
+    	DateTime date = new DateTime();
+    	
+    	User root = new User(this, "root", "root", "rootroot", array, "root", true);
+    	Directory rootDir = new Directory(root, "root", "root", getCounter()+1, date, 2, false, false, false, false);
+    	//no momento de criacao vai ter dimensao 2: "." e ".." - as diretorias sempre presentes
+    	setWorkingDir(rootDir); //define a diretoria de trabalho atual (a ser modificada conforme o user queira mudar)
+    }
+    
+    public void setWorkingDir(Directory dir){
+    	this.workingDir = dir;
+    }
+    
+    public Directory getWorkDir(){
+    	return this.workingDir;
+    }
+    
+  // funcao para teste (ler comandos de input e chamar funcoes a partir dai------------------------------------------------------------
+    public void testLoop(){
+    	
+    	Scanner keyboardSc = new Scanner(System.in);
+    	String input;
+    	while(true){
+    		input = keyboardSc.next();
+    		if(input.equals("Sair\n")){break;}
+    	}
+    }
 	
+     //fim--------------------------------------------------------------------
+    
+    
     public static FileSystem getInstance(){
     	FileSystem fs = FenixFramework.getDomainRoot().getFilesystem();
     	if(fs != null)
