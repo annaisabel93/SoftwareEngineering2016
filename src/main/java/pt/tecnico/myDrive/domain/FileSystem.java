4fff5fd3f39b30af5920a84d00a9089a3ac8722a
Ana Pardal <ist175202@tecnico.ulisboa.pt>
2016-03-08 00:56:52 +0000

    [UPDATE] construtor da classe User e FileSystem

--- src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
+++ src/main/java/pt/tecnico/myDrive/domain/FileSystem.java
@@ -1,9 +1,26 @@
 package pt.tecnico.myDrive.domain;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.FenixFramework;
+
 public class FileSystem extends FileSystem_Base {
+    static final Logger log = LogManager.getRootLogger();
+	
+    public static FileSystem getInstance(){
+    	FileSystem fs = FenixFramework.getDomainRoot().getFilesystem();
+    	if(fs != null)
+    		return fs;
+    	else{
+    		log.trace("new FileSystem");
+    		return new FileSystem(0);
+    	}
+    }
     
-    public FileSystem() {
-        super();
+    private FileSystem(int counter) {
+        setRoot(FenixFramework.getDomainRoot());
+        setCounter(counter);
     }
     
 }
--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -2,8 +2,23 @@ package pt.tecnico.myDrive.domain;
 
 public class User extends User_Base {
     
-    public User() {
+    public User(FileSystem filesystem, String name, String username, String password, byte[] mask, String homeDir, boolean isRoot) {
         super();
+        setName(name);
+        setUserName(username);
+        setPassword(password);
+        setMask(mask);
+        setHomeDir(homeDir);
+        setIsRoot(isRoot);
+        setSystem(filesystem);
     }
     
+    @Override
+    public void setSystem(FileSystem fs){
+    	if(fs == null)
+    		super.setSystem(null);
+    	else{
+    		fs.addUser(this);
+    	}
+    }
 }
