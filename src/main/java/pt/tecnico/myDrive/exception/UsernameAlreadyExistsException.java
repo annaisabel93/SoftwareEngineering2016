6287b302fff0cef26cff5d963dd1dd04c4bef238
Ana Pardal <ist175202@tecnico.ulisboa.pt>
2016-03-09 01:20:50 +0000

    [ADD] excepcoes

---
+++ src/main/java/pt/tecnico/myDrive/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,22 @@
+package pt.tecnico.myDrive.exception;
+
+public class UsernameAlreadyExistsException extends MyDriveException {
+	
+	    private static final long serialVersionUID = 1L;
+
+	    private String conflictingUsername;
+
+	    public UsernameAlreadyExistsException(String conflictingUsername) {
+	        conflictingUsername = conflictingUsername;
+	    }
+
+	    public String getConflictingUsername() {
+	        return conflictingUsername;
+
+	    }
+
+	    @Override
+	    public String getMessage() {
+	        return "This username " + conflictingUsername + " is already being used";
+	    }
+}
