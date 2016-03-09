7339e7dd20492126cbb4818dea4ced541d6f8781
annaisabel93 <agalvao12@hotmail.com>
2016-03-09 22:57:13 +0000

    added xml on File.java, but it has an existencial crisis - read comments

--- src/main/java/pt/tecnico/myDrive/domain/File.java
+++ src/main/java/pt/tecnico/myDrive/domain/File.java
@@ -2,6 +2,11 @@ package pt.tecnico.myDrive.domain;
 
 import org.joda.time.DateTime;
 
+import java.io.UnsupportedEncodingException;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+
 public class File extends File_Base {
     
    public File(FileSystem filesystem, String filename, String owner, long id, DateTime lastModified, int dimension, boolean read, boolean write, boolean delete, boolean execute) {
@@ -29,5 +34,26 @@ public class File extends File_Base {
 			fs.addFile(this);
 		}
 	}
-    
+	
+	public void xmlImport(Document filedoc){
+		try {
+			setFilename(new String(filedoc.getRootElement().getAttribute("filename").getValue().getBytes("UTF-8")));
+			setOwner(new String(filedoc.getRootElement().getAttribute("owner").getValue().getBytes("UTF-8")));
+			setRead(new Boolean(filedoc.getRootElement().getAttribute("read").getValue())); //oh booleans, what are we going to do with you
+			setWrite(new Boolean(filedoc.getRootElement().getAttribute("write").getValue()));
+			setDelete(new Boolean(filedoc.getRootElement().getAttribute("delete").getValue()));
+			setExecute(new Boolean(filedoc.getRootElement().getAttribute("execute").getValue()));
+		} catch (UnsupportedEncodingException e) {
+			System.err.println(e);
+		}
+	}
+	
+	public Document xmlExport(){
+		    	Element element = new Element("File");
+		    	element.setAttribute("filename", getFilename()); 	
+		    	element.setAttribute("owner", getOwner());
+		    	//how to add booleans if setAttribute needs strings?
+		    	Document document = new Document(element);
+				return document;    
+	}
 }
