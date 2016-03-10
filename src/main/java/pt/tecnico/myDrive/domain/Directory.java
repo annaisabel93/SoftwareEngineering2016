6f1ef7e593b3b0d977e045768c2d3f45a8090281
Ana Pardal <ist175202@tecnico.ulisboa.pt>
2016-03-10 10:16:19 +0000

    [UPDATE] xmlImport e xmlExport na classe Directory

--- src/main/java/pt/tecnico/myDrive/domain/Directory.java
+++ src/main/java/pt/tecnico/myDrive/domain/Directory.java
@@ -1,5 +1,7 @@
 package pt.tecnico.myDrive.domain;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
 import org.joda.time.DateTime;
 
 public class Directory extends Directory_Base {
@@ -11,11 +13,21 @@ public class Directory extends Directory_Base {
         setUser(user);
     }
 
-
+    public Directory(User user, Document xml){
+    	xmlImport(xml);
+    	setUser(user);
+    }
 
 	public void addDir(Directory dir){
 		addFile(dir);
 		
 	}
+	public void xmlImport(Document directoryDoc){
+		super.xmlImport(directoryDoc);
+	}
 	
+	public Document xmlExport(){
+		Document directoryDoc = super.xmlExport();
+		return directoryDoc;
+	}
 }
