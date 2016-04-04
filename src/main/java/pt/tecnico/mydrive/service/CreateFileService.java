2e61dfc2cd0f49171b9c865bb504b4ddf2171d6c
Ana Pardal <ist175202@tecnico.ulisboa.pt>
2016-04-04 17:37:56 +0100

    [ADD] Esqueleto do servico CreateFileService.java

---
+++ src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
@@ -0,0 +1,13 @@
+package pt.tecnico.mydrive.service;
+
+import pt.tecnico.mydrive.domain.User;
+
+public class CreateFileService extends FileSystemService{
+	
+	public CreateFileService(String filename, String typeFile){} //---FIXME
+	
+	@Override
+	public final void dispatch(){
+		//--FIXME
+	}
+}
