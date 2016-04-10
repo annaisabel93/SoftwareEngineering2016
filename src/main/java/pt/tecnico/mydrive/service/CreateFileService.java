b7410df324150c7d0fc2ab61ab9f37c9f62a989f
29XA714m35 <joanaraquel1@hotmail.com>
2016-04-11 00:41:33 +0100

    CreateFileService[UPDATE]

--- src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
@@ -1,5 +1,6 @@
 package pt.tecnico.mydrive.service;
 
+import pt.tecnico.mydrive.domain.FileSystem;
 import pt.tecnico.mydrive.domain.User;
 import pt.tecnico.mydrive.domain.Entity;
 import pt.tecnico.mydrive.domain.Directory;
@@ -7,66 +8,68 @@ import pt.tecnico.mydrive.domain.PlainFile;
 import pt.tecnico.mydrive.domain.App;
 import pt.tecnico.mydrive.domain.Link;
 
-
 import org.joda.time.DateTime;
-//import exceptions
 
 import pt.tecnico.mydrive.exception.ContentCannotBeNullException;
-
+import pt.tecnico.mydrive.exception.DirectoryCannotHaveContentException;
 
 public class CreateFileService extends FileSystemService{
 
 	private long token;	
-	private Directory parent;
 	private String fileName;
-	private long id;
-	private DateTime lastModified;
+	private String type;	
 	private String content;
-	private String type;
+	private FileSystem fs;
+	private User user;
+	private Directory workingDir;
+	private long id;
 
-	public CreateFileService(long token, Directory parent, String fileName, long id, DateTime lastModified, String type){
+	public CreateFileService(long token, String fileName, String type){
 		this.token = token;
-		this.parent = parent;	
 		this.fileName = fileName;
-		this.id = id;
-		this.lastModified = lastModified;
 		this.type = type;
+		this.user = this.getLogin(token).getUser();
+		this.workingDir = this.getLogin(token).getDirectory();
+		this.id = this.getFileSystem().getCounter();
 		this.content = "";
 	}
 
-	public CreateFileService(long token, Directory parent, String fileName, long id, DateTime lastModified, String type, String content) {
-		this(token, parent, fileName, id, lastModified, type);
+	public CreateFileService(long token, String fileName, String type, String content) {
+		this(token, fileName, type);
 		this.content = content;
 	}
 
 	
 	
 	@Override
-	public final void dispatch() throws ContentCannotBeNullException {
-		User u = getLogin(token).getUser();
+	public final void dispatch() throws ContentCannotBeNullException, DirectoryCannotHaveContentException {	
+
 		if (content.length() == 0) {
 			if (type.equals("Directory")) {
-				new Directory(parent, fileName, u, id, lastModified);
+				new Directory(this.workingDir, this.fileName, this.user, this.id, new DateTime());
 			}
 			if (type.equals("App")){
-				new App(parent, fileName, u, id, lastModified, null);
+				new App(this.workingDir, this.fileName, this.user, this.id, new DateTime() , null);
 			}
 			if (type.equals("PlainFile")) {
-				new PlainFile(parent, fileName, u, id, lastModified, null);
+				new PlainFile(this.workingDir, this.fileName, this.user, this.id, new DateTime(), null);
 			}
 			if (type.equals("Link")){
 				throw new ContentCannotBeNullException(content); 
 			}
 		}
 		else {
+			if (type.equals("Directory")) {
+				throw new DirectoryCannotHaveContentException(content);
+			}
 			if (type.equals("Link")) {
-			   new Link(parent, fileName, u, id, lastModified, content);
+			   new Link(this.workingDir, this.fileName, this.user, this.id, new DateTime(), content);
 		      	}
 		      	if (type.equals("App")) { 
-			   new App(parent, fileName, u, id, lastModified, content);
+			   new App(this.workingDir, this.fileName, this.user, this.id, new DateTime(), content);
 		      	}
 		      	if(type.equals("PlainFile")) { 
-		           new PlainFile(parent, fileName, u, id, lastModified, content);
+		           new PlainFile(this.workingDir, this.fileName, this.user, this.id, new DateTime(), content);
 		       	}	
 		     }
 	        }
