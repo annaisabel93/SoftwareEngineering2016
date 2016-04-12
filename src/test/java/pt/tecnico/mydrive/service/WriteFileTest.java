0998a8678adf9a6652ac34c08333fbd6f57f520d
filiparsc <filipa.rscosta@gmail.com>
2016-04-12 15:32:44 +0100

    [UPDATE] WriteFileTest

--- src/test/java/pt/tecnico/mydrive/service/WriteFileTest.java
+++ src/test/java/pt/tecnico/mydrive/service/WriteFileTest.java
@@ -12,29 +12,36 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import pt.tecnico.mydrive.Main;
+import pt.tecnico.mydrive.domain.Entity;
 import pt.tecnico.mydrive.domain.FileSystem;
+import pt.tecnico.mydrive.domain.Login;
 import pt.tecnico.mydrive.domain.PlainFile;
 import pt.tecnico.mydrive.domain.User;
-import pt.tecnico.mydrive.exception.TexFileDoesNotExistException;
+import pt.tecnico.mydrive.exception.EntityDoesNotExistException;
+import pt.tecnico.mydrive.exception.UserHasInvalidPermissionsException;
 
-public class WriteFileTest {
+public class WriteFileTest extends AbstractServiceTest{
+
+	private long token;
 
 	protected void populate() {
 		
 		FileSystem fs = FileSystem.getInstance();
-		
-		byte[] mask = {1, 1, 0, 0};
+	
+		byte[] mask = {1, 1, 1, 1};
 		String username = "filipac";
 		String name = username;
 		String password = username;
 		String homeDir = "/home/" + username;
 		User user = new User(fs, name, username, password, mask, homeDir);
+		LoginService service = new LoginService(username, username);
+		service.execute();
 		
-		//Token token = new Token();
-		Long token = 100L;
+		this.token =  service.getToken();
+		Login login = user.getLoginbyToken(service.getToken());
 		
 		String filename = "testFile";
-		long id = 1000L;
+		long id = 1;
 		DateTime lastModified = new DateTime();
 		String content = "content";
 		PlainFile file = new PlainFile(fs.getUserDir(username), filename, user, id, lastModified, content);
@@ -43,18 +50,31 @@ public class WriteFileTest {
 	
 	@Test
 	public void success() {
-//		WriteFileService service = new WriteFileService(100L, "testFile", "testContent");
-//		service.execute;
-//		PlainFile file = service.result();
-//		
-//		assertEquals(file.getContent(), "testContent");
+		FileSystem fs = FileSystem.getInstance();
+		
+		WriteFileService writetext = new WriteFileService(this.token, "testFile", "content");
+		writetext.execute();
+		
+		//checks
+		
+		Entity fil = fs.getUserByUsername("filipac").getHome().getByName("file");
+        assertNull("File content ", fil);
+        
+		//PlainFile file = service.result();
+		//assertEquals(file.getContent(), "testContent");
 	}
 	
-//	@Test(expected = TexFileDoesNotExistException.class)
+//	@Test(expected = EntityDoesNotExistException.class)
 //	public void invalidWriteFileWithNonexistingFilename() {
-//		WriteFileService service = new WriteFileService(101L, "noFile", "content");
+//		WriteFileService service = new WriteFileService(this.token, "noFile", "content");
 //		service.execute();
 //	}
 	
-	//Se utilizador nao tiver permissoes para escrever no ficheiro dado (tb falta criar a respectiva excepcao)
+	
+	//@Test(expected = UserHasInvalidPermissionsException.class)
+	//public void invalidWriteFileWithNonexistingFilename() {
+	//	WriteFileService service = new WriteFileService(101L, "noFile", "content");
+	//	service.execute();
+	//}
+	//Se utilizador nao tiver permissoes para escrever no ficheiro dado (excepcao--> check)
 }
