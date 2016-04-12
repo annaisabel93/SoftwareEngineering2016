cc7a24ba80150dd125cc7974526f85a55a238c26
29XA714m35 <joanaraquel1@hotmail.com>
2016-04-12 02:50:56 +0100

    CreateFileService.Minimizing.[UPDATE]

--- src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/CreateFileService.java
@@ -1,12 +1,12 @@
 package pt.tecnico.mydrive.service;
 
 import pt.tecnico.mydrive.domain.FileSystem;
-import pt.tecnico.mydrive.domain.User;
 import pt.tecnico.mydrive.domain.Entity;
 import pt.tecnico.mydrive.domain.Directory;
 import pt.tecnico.mydrive.domain.PlainFile;
 import pt.tecnico.mydrive.domain.App;
 import pt.tecnico.mydrive.domain.Link;
+import pt.tecnico.mydrive.domain.User;
 
 import org.joda.time.DateTime;
 
@@ -20,11 +20,12 @@ public class CreateFileService extends FileSystemService{
 	private long token;	
 	private String fileName;
 	private String type;	
-	private String content;
+	
 	private FileSystem fs;
-	private User user;
 	private Directory workingDir;
+	private User user;
 	private DateTime lastModified;
+	private String content;
 
 
 	public CreateFileService(long token, String fileName, String type){
@@ -42,52 +43,49 @@ public class CreateFileService extends FileSystemService{
 		this.content = content;
 	}
 
-	public int incCounter(){
+	private int incCounter(){
 		FileSystem fs = this.user.getSystem();
 		int count = fs.getCounter();
 		fs.setCounter( count++ );
 		return count;
 	}
 
+	private void createCaseContent(boolean isContentNull) {
+		switch(this.type) {
+			case "Directory":
+				if (isContentNull) {
+					new Directory(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified);
+					break;
+				}else{ throw new DirectoryCannotHaveContentException(content); }
+			case "App":
+				if (isContentNull) { this.content = null; }
+				new App(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
+				break;
+
+			case "PlainFile":
+				if (isContentNull) { this.content = null; }
+				new PlainFile(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
+				break;
+			case "Link":
+				if (isContentNull) { throw new ContentCannotBeNullException(content); }
+				else{
+					new Link(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
+					break;
+
+				}
+			default:
+				throw new UnknownFileTypeException(type);
+		}
+	}
+
+	
 	//verify if receives path or not	
 	@Override
 	public final void dispatch() throws ContentCannotBeNullException, DirectoryCannotHaveContentException, UnknownFileTypeException {	
 
-		if (content.length() == 0) {
-			switch(type) {
-				case "Directory":
-					new Directory(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified);
-					break;
-				case "App":
-					new App(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified , null);
-					break;
-				case "PlainFile":
-					new PlainFile(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, null);
-					break;
-				case "Link":
-					throw new ContentCannotBeNullException(content); 
-				default:
-					throw new UnknownFileTypeException(type);
-			}
-		}
-		else {
-			switch(type) {
-				case "Link":
-			   		new Link(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
-					break;
-		      		case "App":
-			   		new App(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
-					break;
-		      		case "PlainFile":
-		           		new PlainFile(this.workingDir, this.fileName, this.user, incCounter(), this.lastModified, content);
-					break;
-		       		case "Directory":
-					throw new DirectoryCannotHaveContentException(content);
-				default:
-					throw new UnknownFileTypeException(type);
-			}	
-		
-	        }
+		if (content.length() == 0) { createCaseContent(true); }
+		else { createCaseContent(false); }
 
 	}
+
 }
