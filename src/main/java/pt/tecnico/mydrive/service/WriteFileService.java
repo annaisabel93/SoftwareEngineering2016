9020f5b7e7820fa6957b2daeb45e0987d34a5d5e
filiparsc <filipa.rscosta@gmail.com>
2016-04-13 19:03:15 +0100

    [FIX] WriteFileService (permissions)

--- src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
+++ src/main/java/pt/tecnico/mydrive/service/WriteFileService.java
@@ -3,20 +3,29 @@ package pt.tecnico.mydrive.service;
 import pt.tecnico.mydrive.domain.Login;
 import pt.tecnico.mydrive.domain.PlainFile;
 import pt.tecnico.mydrive.exception.TexFileDoesNotExistException;
+import pt.tecnico.mydrive.exception.UserHasInvalidPermissionsException;
 
 public class WriteFileService extends FileSystemService {
-	
+	Login login;
 	private long token;
 	private String fileName;
 	private String content;
 	
     public WriteFileService(long token, String fileName, String content) {
     	this.token = token;
+    	this.login = getLogin(token);
         this.fileName = fileName;
+        checkPermissions(login.getUser().getMask());
         }
 
+    public void checkPermissions(byte[] permissions) throws UserHasInvalidPermissionsException {
+		if(permissions[1] == 0) {
+			throw new UserHasInvalidPermissionsException();
+		}
+	}
+    
     @Override
-    public final void dispatch() throws TexFileDoesNotExistException{
+    public final void dispatch() throws TexFileDoesNotExistException, UserHasInvalidPermissionsException{
     	
         Login login = getLogin(token);
         PlainFile text = (PlainFile) login.getDirectory().getByName(this.fileName);
