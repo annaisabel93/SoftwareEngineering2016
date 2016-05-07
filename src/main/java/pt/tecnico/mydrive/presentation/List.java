4a24e93e204630f95429937856ec455429de303a
filiparsc <filipa.rscosta@gmail.com>
2016-05-07 23:51:38 +0100

    [UPDATE] list command updated

--- src/main/java/pt/tecnico/mydrive/presentation/List.java
+++ src/main/java/pt/tecnico/mydrive/presentation/List.java
@@ -1,15 +1,35 @@
 package pt.tecnico.mydrive.presentation;
-// imports missing
+
+import java.math.BigInteger;
+import java.util.Random;
+
+import pt.tecnico.mydrive.service.ChangeDirectoryService;
+import pt.tecnico.mydrive.service.ListDirectoryService;
 
 public class List extends Command {
 
+	private Shell sh;
+	
 	public List(Shell sh) {
-    	super(sh, "list", "lists the files on the current directory, or, if given the path of the directory, lists the files in that directory");
+    	super(sh, "ls", "lists the files on the current directory, or, if given the path of the directory, lists the files in that directory");
+    	this.sh = sh;
     }
 
 	public void execute(String[] args) {
-		
-		
+		//long token = sh.getToken();  token bem feito
+//		long token = new BigInteger(64, new Random()).longValue(); // token temporario
+//		
+//		if (args.length == 0) {
+//			ListDirectoryService lds = new ListDirectoryService(token);
+//			lds.execute();
+//		} else if(args.length == 1) {
+//			ChangeDirectoryService cds = new ChangeDirectoryService(token, args[0]);
+//			cds.execute();
+//			ListDirectoryService lds = new ListDirectoryService(token);
+//			lds.execute();
+//		} else {
+//			throw new RuntimeException("USAGE: [path]");
+//		}
 	}
 
 	
