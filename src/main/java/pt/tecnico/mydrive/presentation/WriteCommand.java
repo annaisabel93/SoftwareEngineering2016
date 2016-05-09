8517ef72ad07b97afabf130ed692667ed6668acb
filiparsc <filipa.rscosta@gmail.com>
2016-05-09 11:41:37 +0100

    [UPDATE] WriteCommand body

--- src/main/java/pt/tecnico/mydrive/presentation/WriteCommand.java
+++ src/main/java/pt/tecnico/mydrive/presentation/WriteCommand.java
@@ -13,6 +13,14 @@ public class WriteCommand extends Command {
 	}
 
 	public void execute(String[] args) {
-
+		//long token = sh.getToken();  token bem feito
+//		long token = new BigInteger(64, new Random()).longValue(); // token temporario
+//
+//		if (args.length == 2) {
+//			WriteFileService wfs = new WriteFileService(token, args[0], args[1]);
+//			wfs.execute();
+//		} else {
+//			throw new RuntimeException("USAGE: <update> <path> <text>");
+//		}
 	}
 }
