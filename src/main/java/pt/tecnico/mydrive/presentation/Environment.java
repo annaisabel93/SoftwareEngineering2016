932ea7bdf226a56ea66e188ad5d5774701ec6104
29XA714m35 <joanaraquel1@hotmail.com>
2016-05-10 00:15:43 +0100

    Added Environment and Execute skeleton files.

---
+++ src/main/java/pt/tecnico/mydrive/presentation/Environment.java
@@ -0,0 +1,17 @@
+package pt.tecnico.mydrive.presentation;
+import pt.tecnico.mydrive.service.AddVariableService;
+
+public class Environment extends MyDriveCommand {
+
+	public Environment (Shell sh) { super(sh, "env", "creates or modifies an environment variable"); }
+	public void execute (String[] args) {
+	}
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/pt/tecnico/mydrive/presentation/Execute.java
@@ -0,0 +1,13 @@
+package pt.tecnico.mydrive.presentation;
+import pt.tecnico.mydrive.service.ExecuteFileService;
+
+public class Execute extends MyDriveCommand {
+
+	public Execute (Shell sh) { super(sh, "do path", "executes file in indicated path"); }
+	public void execute (String[] args) {
+	}
+
+
+
+
+}
