38d10b4b4a25b32c041facaeba5a54ad4d5bdd11
29XA714m35 <joanaraquel1@hotmail.com>
2016-05-13 16:40:01 +0100

    Last commands not working

--- src/main/java/pt/tecnico/mydrive/presentation/ExecuteCommand.java
+++ src/main/java/pt/tecnico/mydrive/presentation/ExecuteCommand.java
@@ -2,9 +2,26 @@ package pt.tecnico.mydrive.presentation;
 import pt.tecnico.mydrive.service.ExecuteFileService;
 
 public class ExecuteCommand extends MyDriveCommand {
+	
+	private Shell sh;
 
-	public ExecuteCommand (Shell sh) { super(sh, "do path", "executes file in indicated path"); }
+	public ExecuteCommand (Shell sh) { 
+		super(sh, "do", "executes file in indicated path"); 
+		this.sh = sh;
+	}
+
 	public void execute (String[] args) {
+		long token = sh.getToken();
+		if ( args.length < 1 ) {
+			throw new RuntimeException("USAGE: <do> <path> [<args>}");
+		}
+		else if ( args.length == 1 ) {
+			//new ExecuteFileService(token, args[0], null).execute();
+		}
+		else {
+			//new ExecuteFileService(token, args[0], args[1]).execute();
+		}
+
 	}
 
 
